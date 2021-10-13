af98991c246cf42b12073f5a9e628eea19abb168
eve9te <eve9te@mail.ru>
2021-10-13 13:37:23 +0300

    fix lab1 - Brich

--- reports/Brych_Maxim/lab1/src/Lab1.java
+++ reports/Brych_Maxim/lab1/src/Lab1.java
@@ -3,14 +3,14 @@ import java.util.Random;
 public class Lab1 {
     
     public static void main(String[] args) {
-        Task1();
-        Task2(Integer.parseInt(args[0]));
-        Task3(args[1]);
+        Task1(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        Task2(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+        Task3(args[4]);
     }
 
-    public static void Task1() {
+    public static void Task1(int val, int min, int max) {
         System.out.println("\nЗадание №1:");
-        int val = 20, min = 1, max = 20, trend = 0, max_count = 0;
+        int trend = 0, max_count = 0;
         int[] row = new int[val];
         Random rand = new Random();
 
@@ -28,22 +28,22 @@ public class Lab1 {
                 trend = row[i];
             } else if(count == max_count) trend = 0;
         }
+        
         if (trend != 0)
             System.out.printf("\n moda - %d, count - %d \n", trend, max_count);
         else 
             System.out.println("\n no moda \n");
     }
 
-    public static void Task2(int step) {
+    public static void Task2(int val, int min, int max, int step) {
         System.out.println("\nЗадание №2:");
-        int val = 20, min = 100, max = 2000;
         Double [] array = new Double[20];
         Random rand = new Random();
 
         System.out.println("Последовательность:");
         for(int i = 0; i < val; i++) { // создание ряда
             new Random();
-            array[i] = Double.valueOf(rand.nextInt((max - min + 1)) / 10);
+            array[i] = Double.valueOf(rand.nextInt((max - min + 1))) / 10;
             System.out.print(array[i] + " ");
         }
 
