bd9e7fccd781a359f600e91870a0a34f7f90e685
Radionchik Svetlana <greenbay654@gmail.com>
2021-10-18 21:09:51 +0300

    Lab 3

---
+++ reports/Радиончик Светлана Сергеевна/Lab 3/src/Task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab3_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Set set1 = new Set(new double[] { -2, 3.5, 4, 7 });
+			Set set2 = new Set(set1.set);
+			set1.Print();
+			set2.Print();
+
+			set1.AddElement(10);
+			set1.Print();
+			set2.DeleteElement(3.5);
+			set2.Print();
+
+			Console.WriteLine(set1.Equals(set2));
+
+			Set set3 = set1.CombineSet(set2);
+			set3.Print();
+
+			Console.Write(set1.ToString());
+		}
+	}
+}
---
+++ reports/Радиончик Светлана Сергеевна/Lab 3/src/Task 1/Set.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab3_1
+{
+	class Set
+	{
+		public double[] set;
+
+		public Set(int N)
+		{
+			this.set = new double[N];
+		}
+
+		public Set(double[] set)
+		{
+			this.set = new double[set.Length];
+			this.set = set;
+		}
+
+		public Set CombineSet(Set addedSet)
+		{
+			Set combineSet = new Set(this.set);
+
+			for(int i = 0; i < addedSet.set.Length; i++)
+			{
+				if (!combineSet.set.Any(x => x == addedSet.set[i]))
+					combineSet.AddElement(addedSet.set[i]);
+			}
+
+			combineSet.set = Sort(combineSet.set);
+
+			return combineSet;
+		}
+
+		public void Print()
+		{
+			Console.Write("Set: ");
+			for(int i = 0; i < this.set.Length; i++)
+			{
+				Console.Write($"{this.set[i]}");
+				if (i != this.set.Length - 1)
+					Console.Write(", ");
+			}
+				
+			Console.WriteLine();
+		}
+
+		public void AddElement(double element)
+		{
+			if(!this.IsSetElement(element))
+			{
+				Set changedSet = new Set(this.set.Length + 1);
+
+				for(int i = 0; i < this.set.Length; i++)
+				{
+					changedSet.set[i] = this.set[i];
+				}
+
+				changedSet.set[this.set.Length] = element;
+
+				this.set = Sort(changedSet.set);
+			}
+		}
+
+		public void DeleteElement(double element)
+		{
+			if (this.IsSetElement(element))
+			{
+				Set changedSet = new Set(this.set.Length - 1);
+
+				for (int i = 0, j = 0; i < this.set.Length; i++, j++)
+				{
+					if (this.set[i] == element)
+					{
+						j--;
+					}
+					else
+						changedSet.set[j] = this.set[i];
+				}
+
+				this.set = changedSet.set;
+			}
+			else
+				Console.WriteLine("Нет такого элемента");
+		}
+
+		public bool Equals(Set comparableSet)
+		{
+			if (this.set.Length != comparableSet.set.Length)
+				return false;
+
+			for (int i = 0; i < this.set.Length; i++)
+				if (this.set[i] != comparableSet.set[i])
+					return false;
+
+			return true;
+		}
+
+		public override string ToString()
+		{
+			string result = "";
+
+			foreach (double x in this.set)
+				result += $"{x.ToString()} ";
+
+			return result;
+		}
+
+
+		private bool IsSetElement(double element)
+		{
+			if (this.set.Any(x => x == element))
+				return true;
+			return false;
+		}
+
+		private double[] Sort(double[] set)
+		{
+			double temp;
+			
+			for(int i = 0; i < set.Length - 1; i++)
+			{
+				for (int j = i + 1; j < set.Length; j++)
+				{
+					if(set[i] > set[j])
+					{
+						temp = set[i];
+						set[i] = set[j];
+						set[j] = temp;
+					}
+				}
+			}
+
+			return set;
+		}
+	}
+}
