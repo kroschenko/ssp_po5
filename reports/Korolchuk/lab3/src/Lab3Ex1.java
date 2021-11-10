d7210b7a68cc829eb661ec216f0ddd5a4b0deb69
KaffLime <artem.korolchuk.02@mail.ru>
2021-11-10 15:37:05 +0300

    Korolchuk: modified Lab3Ex1

--- reports/Korolchuk/lab3/src/Lab3Ex1.java
+++ reports/Korolchuk/lab3/src/Lab3Ex1.java
@@ -6,16 +6,17 @@ import java.util.Arrays;
 public class Lab3Ex1 {
     public static void main(String[] args) {
         char[] mass = {'a', 'b', 'c'};
-        Symbols obj1 = new Symbols(mass);
+        Symbols obj1 = new Symbols(mass, mass.length);
         
-        Symbols obj2 = new Symbols(mass);
+        Symbols obj2 = new Symbols(mass, 3);
         
         Symbols obj3 = new Symbols(5);
         
         System.out.println(obj1.equals(obj2));
         
-        obj2.add('d');
         obj2.remove('a');
+        obj2.add('d');
+        obj1.add('b');
         
         obj2.print();
         System.out.println(obj2);
@@ -46,25 +47,41 @@ public class Lab3Ex1 {
 class Symbols {
     
     private char[] charSet;
+    private int maxSize;
     
     public Symbols(int length) {
-        this.charSet = new char[length];
+        this.maxSize = length;
+        this.charSet = new char[0];
     }
     
-    public Symbols(char[] charSet) {
+    public Symbols(char[] charSet, int length) {
+        HashSet<Character> charHashSet = convertHash(charSet);
+
+        if (charHashSet.size() <= length) {
+            this.maxSize = length;
+            this.charSet = new char[charHashSet.size()];
+
+            int count = 0;
+            for (char i : charHashSet) {
+                this.charSet[count] = i;
+                count++;
+            }
+        }
+        else {
+            System.out.println("Длина множества превышает указанный максимум");
+            this.maxSize = length;
+            this.charSet = new char[0];
+        }
+    }
+    
+    public HashSet<Character> convertHash(char[] charSet) {
         HashSet<Character> charHashSet = new HashSet<>();
-        
+
         for (int i = 0; i < charSet.length; i++) {
             charHashSet.add(charSet[i]);
         }
         
-        this.charSet = new char[charHashSet.size()];
-        
-        int count = 0;
-        for (char i : charHashSet) {
-            this.charSet[count] = i;
-            count++;
-        }
+        return charHashSet;
     }
     
     public char[] getCharSet() {
@@ -72,18 +89,19 @@ class Symbols {
     }
     
     public void setCharSet(char[] charSet) {
-        HashSet<Character> charHashSet = new HashSet<>();
-        
-        for (int i = 0; i < charSet.length; i++) {
-            charHashSet.add(charSet[i]);
+        HashSet<Character> charHashSet = convertHash(charSet);
+            
+        if (charHashSet.size() <= this.maxSize) {
+            this.charSet = new char[charHashSet.size()];
+
+            int count = 0;
+            for (char i : charHashSet) {
+                this.charSet[count] = i;
+                count++;
+            }
         }
-        
-        this.charSet = new char[charHashSet.size()];
-        
-        int count = 0;
-        for (char i : charHashSet) {
-            this.charSet[count] = i;
-            count++;
+        else {
+            System.out.println("Недопустимая длина множества, превышен максимум");
         }
     }
     
@@ -97,22 +115,23 @@ class Symbols {
     }
     
     public void append(Symbols obj) {
-        HashSet<Character> charHashSet = new HashSet<>();
-        
-        for (int i = 0; i < this.charSet.length; i++) {
-            charHashSet.add(this.charSet[i]);
-        }
-        
+        HashSet<Character> charHashSet = convertHash(charSet);
+
         for (int i = 0; i < obj.charSet.length; i++) {
             charHashSet.add(obj.charSet[i]);
         }
-        
-        this.charSet = new char[charHashSet.size()];
-        
-        int count = 0;
-        for (char i : charHashSet) {
-            this.charSet[count] = i;
-            count++;
+            
+        if (charHashSet.size() <= this.maxSize) {
+            this.charSet = new char[charHashSet.size()];
+
+            int count = 0;
+            for (char i : charHashSet) {
+                this.charSet[count] = i;
+                count++;
+            }
+        }
+        else {
+            System.out.println("Множество, получаемое в результате объединения, имеет длину, превышающую максимальную");
         }
     }
     
@@ -140,20 +159,21 @@ class Symbols {
     }
     
     public void add(char symbol) {
-        HashSet<Character> charHashSet = new HashSet<>();
-        
-        for (int i = 0; i < this.charSet.length; i++) {
-            charHashSet.add(this.charSet[i]);
-        }
-        
+        HashSet<Character> charHashSet = convertHash(charSet);
+
         charHashSet.add(symbol);
-        
-        this.charSet = new char[charHashSet.size()];
-        
-        int count = 0;
-        for (char i : charHashSet) {
-            this.charSet[count] = i;
-            count++;
+
+        if (charHashSet.size() <= this.maxSize) {
+            this.charSet = new char[charHashSet.size()];
+
+            int count = 0;
+            for (char i : charHashSet) {
+                this.charSet[count] = i;
+                count++;
+            }
+        }
+        else {
+            System.out.println("Множество достигло максимальной длины");
         }
     }
     
