6490d5197f264af1108e0b03de2068a64aee8f44
vitas26072001 <vitas20011@inbox.ru>
2022-05-03 14:35:20 +0300

    Fixed error lab8

--- reports/Kroshchuk Viktor/Lab8/src/Calculate.java
+++ reports/Kroshchuk Viktor/Lab8/src/Calculate.java
@@ -11,6 +11,7 @@ public class Calculate extends Application {
     double currentValue = 0;
     int currentIteration = 0;
     Text sum = new Text("");
+    Thread backgroundThread;
     TextField inputCount = new TextField();
     TextField inp = new TextField();
     TextField in = new TextField();
@@ -19,10 +20,16 @@ public class Calculate extends Application {
 
     @Override
     public void init() {
-        start.setText("Вычислить");
+        start.setText("Начать");
+        Button pause = new Button();
+        pause.setText("Пауза");
         Button stop = new Button();
         stop.setText("Очистить");
         start.setOnAction(actionEvent -> startCalculate());
+        pause.setOnAction(actionEvent -> {
+            start.setDisable(false);
+            backgroundThread.suspend(); // приостанавливает
+        });
         stop.setOnAction(actionEvent -> {
             start.setDisable(false);
             stopCalculate();
@@ -45,7 +52,8 @@ public class Calculate extends Application {
         grid.add(labelin, 0,3,1,1);
         grid.add(in, 1,3,1,1);
         grid.add(start, 0, 5);
-        grid.add(stop, 1, 5);
+        grid.add(pause, 1,5);
+        grid.add(stop, 2, 5);
     }
 
     @Override
@@ -57,22 +65,40 @@ public class Calculate extends Application {
     }
 
     public void startCalculate() {
-        int k = Integer.parseInt(inputCount.getText()) + 1;
-        int x = Integer.parseInt(inp.getText());
-        int a = Integer.parseInt(in.getText());
-        start.setDisable(true);
-        if(a == 0)
-            sum.setText("-infinity");
-        if (k == 0) {
-            sum.setText(Double.toString(1.0));
-        } else {
-            sum.setText(Double.toString(this.currentValue));
-            for (int i = 0; i < k; i++) {
-                this.currentValue += Math.pow(x * Math.log(a), i) / factorial(i);
-                sum.setText(Double.toString(this.currentValue));
-            }
+        if (backgroundThread != null) {
+            backgroundThread.resume(); // продолжает
+        } else{
+            Thread task = new Thread(() -> {
+                try {
+                    int k = Integer.parseInt(inputCount.getText()) + 1;
+                    int x = Integer.parseInt(inp.getText());
+                    int a = Integer.parseInt(in.getText());
+                    start.setDisable(true);
+                    if(a == 0)
+                        sum.setText("-infinity");
+                    if (k == 0) {
+                        sum.setText(Double.toString(1.0));
+                    } else {
+                        sum.setText(Double.toString(this.currentValue));
+                        for (int i = 0; i < k; i++) {
+                            try {
+                                this.currentValue += Math.pow(x * Math.log(a), i) / factorial(i);
+                                Thread.sleep(500);
+                                sum.setText(Double.toString(this.currentValue));
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    start.setDisable(false);
+                } catch (NumberFormatException e) {
+                    sum.setText("Ошибка ввода");
+                }
+            });
+            backgroundThread = new Thread(task);
+            backgroundThread.setDaemon(true);
+            backgroundThread.start();
         }
-        start.setDisable(false);
     }
 
     public double factorial(int num){
@@ -85,12 +111,14 @@ public class Calculate extends Application {
     }
 
     public void stopCalculate() {
+        backgroundThread.stop();
         this.currentValue = 0;
         this.sum.setText("");
         this.currentIteration = 0;
         inputCount.setText("");
         inp.setText("");
         in.setText("");
+        backgroundThread = null;
     }
 
     public static void main(String[] args) {
